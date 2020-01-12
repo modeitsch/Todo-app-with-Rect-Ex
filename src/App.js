5cdab022a524adaaf1881cd7c3d5d01ddeb679b9
Moshe Deitsch <me@moshedeitsch.com>
2020-01-12 15:29:40 +0200

    Embed Todos in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Todos from './Components/Todos'
 import './App.css';
 
 class App extends React.Component {
   render() { 
   return (
     <div className="App">
-        <h1>Hello World</h1>
+      <Todos/>
     </div>
   );
 }
