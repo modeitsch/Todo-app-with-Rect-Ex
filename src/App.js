801c601fac37632cdb810091024187d1272572b0
Moshe Deitsch <me@moshedeitsch.com>
2020-01-13 10:45:05 +0200

    all is working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,86 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Todos from './Components/Todos'
 import './App.css';
+import Header from './Components/Lyaout/Header'
+import AddTodo from './Components/AddTodo'
+import About from './Components/pages/About'
+import uuid from 'uuid'
 
 class App extends React.Component {
-  render() { 
-  return (
-    <div className="App">
-      <Todos/>
-    </div>
-  );
-}
+  state = {
+    todos: [
+      {
+        id: uuid.v4(),
+        title: 'Take out the trash',
+        completed: false,
+      },
+      {
+        id: uuid.v4(),
+        title: 'Dinner with wife',
+        completed: true,
+      },
+      {
+        id: uuid.v4(),
+        title: 'Metting with boss',
+        completed: false,
+      }
+    ]
+  }
+
+
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    })
+  }
+
+  addTodo = (title) => {
+
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route path='/' render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos delTodo={this.delTodo} todos={this.state.todos} markComplete={this.markComplete} />
+
+
+              </React.Fragment>
+            )} />
+            <Route path='/about' component={About} />
+
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
